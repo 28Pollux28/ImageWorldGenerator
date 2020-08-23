826f027940e154f0fb89ec50edffec0f0b5c4c07
28Pollux28 <lemaire_valentin28@yahoo.fr>
2020-08-23 15:18:08 +0200

    Added Config

--- src/main/java/eu/pollux28/genmap/GenMap.java
+++ src/main/java/eu/pollux28/genmap/GenMap.java
@@ -1,6 +1,9 @@
 package eu.pollux28.genmap;
 
+import eu.pollux28.genmap.config.GenMapConfig;
 import eu.pollux28.genmap.gen.GenMapType;
+import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
+import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
@@ -8,11 +11,16 @@ import net.fabricmc.loader.api.FabricLoader;
 public class GenMap implements ModInitializer {
 
     public static GenMapType levelGeneratorType;
+    public static GenMapConfig config;
+
 
     @Override
     public void onInitialize() {
         if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT){
             levelGeneratorType = new GenMapType("genmap");
         }
+        AutoConfig.register(GenMapConfig.class, GsonConfigSerializer::new);
+        config = AutoConfig.getConfigHolder(GenMapConfig.class).getConfig();
+
     }
 }
---
+++ src/main/java/eu/pollux28/genmap/config/GenMapConfig.java
@@ -0,0 +1,22 @@
+package eu.pollux28.genmap.config;
+
+import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
+import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
+
+@Config(name = "genmap")
+public class GenMapConfig implements ConfigData {
+    public boolean test = false;
+    boolean toggleA = true;
+    boolean toggleB = false;
+
+    @ConfigEntry.Gui.CollapsibleObject
+    InnerStuff stuff = new InnerStuff();
+
+    @ConfigEntry.Gui.Excluded
+    InnerStuff invisibleStuff = new InnerStuff();
+    static class InnerStuff {
+        int a = 0;
+        int b = 1;
+    }
+}
