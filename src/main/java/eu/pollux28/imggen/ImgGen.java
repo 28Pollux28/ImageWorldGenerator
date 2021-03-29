580c7ec6096e58fe2f484abcfaa08f627705cf6b
28Pollux28 <lemaire_valentin28@yahoo.fr>
2021-03-29 21:29:34 +0200

    v1.1.0

--- src/main/java/eu/pollux28/imggen/ImgGen.java
+++ src/main/java/eu/pollux28/imggen/ImgGen.java
@@ -23,7 +23,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ImgGen implements ModInitializer {
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
     public static final Logger logger = LogManager.getLogger();
     public static MainConfigData CONFIG;
 
