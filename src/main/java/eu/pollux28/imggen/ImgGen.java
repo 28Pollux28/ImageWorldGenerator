1a13e259215d9d3bc7cec359eab3ee081b7e4231
28Pollux28 <lemaire_valentin28@yahoo.fr>
2021-06-30 16:45:53 +0200

    v1.2.0-0 Sea Level fix

--- src/main/java/eu/pollux28/imggen/ImgGen.java
+++ src/main/java/eu/pollux28/imggen/ImgGen.java
@@ -23,7 +23,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ImgGen implements ModInitializer {
-    public static final String VERSION = "1.1.3";
+    public static final String VERSION = "1.2.0-0";
     public static final Logger logger = LogManager.getLogger();
     public static MainConfigData CONFIG;
     public static BiomeColorConverter biomeColorConverter;
