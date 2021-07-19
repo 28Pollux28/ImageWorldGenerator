a1de2f1351e94255534c7a307370f749e6a1037f
28Pollux28 <lemaire_valentin28@yahoo.fr>
2021-07-19 12:49:35 +0200

    v1.2.0-0 Command fix

--- src/main/java/eu/pollux28/imggen/data/BiomeDataProvider.java
+++ src/main/java/eu/pollux28/imggen/data/BiomeDataProvider.java
@@ -70,4 +70,32 @@ public final class BiomeDataProvider {
         int rgb = image.getRGB(centeredX, centeredY);
         return colorConverter.GetValue(rgb);
     }
+
+    public int[] getImageCoordAndValue(int x,int y){
+        int centeredX = (int)((x / (scale)) + width12);
+        int centeredY = (int)((y / (scale)) + height12);
+        if (!isInImage(x,y)){
+            if(!ImgGen.CONFIG.repeatBiomeImage){
+                int rgb = imageManipulator.getRGB(centeredX, centeredY);
+                return new int[]{centeredX, centeredY, rgb};
+            }
+            else {
+                if(!ImgGen.CONFIG.repeatMirrorBiomeImage){
+                    centeredX = centeredX>=0 ? centeredX%width:-width*((centeredX+1)/width)+width+centeredX;
+                    centeredY = centeredY>=0 ? centeredY%height:-height*((centeredY+1)/height)+height+centeredY;
+                }else{
+                    centeredX = centeredX<0? -centeredX-1 : centeredX;
+                    centeredY = centeredY<0? -centeredY-1 : centeredY;
+                    int kX = Math.abs(centeredX/(width))+(centeredX>=0?0:1);
+                    int kY = Math.abs(centeredY/(height))+(centeredY>=0?0:1);
+                    centeredX = kX%2==0 ? centeredX%width : width-1-centeredX%width;
+                    centeredY = kY%2==0 ? centeredY%height : height-1-centeredY%height;
+                }
+                int rgb = imageManipulator.getRGB(centeredX, centeredY);
+                return new int[]{centeredX,centeredY, rgb};
+            }
+        }
+        int rgb = imageManipulator.getRGB(centeredX, centeredY);
+        return new int[]{centeredX,centeredY,rgb&0x00FFFFFF};
+    }
 }
