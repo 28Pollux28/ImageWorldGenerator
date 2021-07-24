c374aa6c203f5f14476fc07a50cdf0dabb77f7c3
28Pollux28 <lemaire_valentin28@yahoo.fr>
2021-07-25 00:00:07 +0200

    v1.3.2 Release pngj dependency fix

--- src/main/java/eu/pollux28/imggen/ImgGen.java
+++ src/main/java/eu/pollux28/imggen/ImgGen.java
@@ -30,7 +30,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ImgGen implements ModInitializer {
-    public static final String VERSION = "1.3.1";
+    public static final String VERSION = "1.3.2";
     public static final Logger logger = LogManager.getLogger();
     public static MainConfigData CONFIG;
     public static BiomeColorConverter biomeColorConverter;
