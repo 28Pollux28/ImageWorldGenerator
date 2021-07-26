33c74f873618b32499c6a966375e62421aacf1d6
28Pollux28 <lemaire_valentin28@yahoo.fr>
2021-07-26 22:51:30 +0200

    v1.3.3 Release Fix cache wrong indexing

--- src/main/java/eu/pollux28/imggen/data/ImageManipulator.java
+++ src/main/java/eu/pollux28/imggen/data/ImageManipulator.java
@@ -69,19 +69,19 @@ public class ImageManipulator {
                 int wSpan = width - wProgress;
                 int minWidth = Math.min(maxWidth, wSpan);
                 IImageLine lint;
-                BufferedImage image = new BufferedImage(minWidth, minHeight,BufferedImage.TYPE_INT_ARGB);
+                BufferedImage image = new BufferedImage(minWidth, minHeight,BufferedImage.TYPE_INT_RGB);
                 int r =hProgress;
                 while (r< hProgress+minHeight) {
                     lint = lines.getImageLine(r);
-                    int[] scanLine = ((ImageLineInt)lint).getScanline();
                     for (int ii = wProgress; ii < wProgress+minWidth; ii++) {
                         int nextPixel = 0;
                         if(channels ==4){
-                            nextPixel = ImageLineHelper.getPixelARGB8(lint,ii);
+                            nextPixel = ImageLineHelper.getPixelARGB8(lint,ii)&0x00FFFFFF; /*|0xff000000;*/
                         }else if(channels==3){
                             nextPixel = ImageLineHelper.getPixelRGB8(lint,ii);
                         }else if(channels==1){
-                            nextPixel = (scanLine[ii] << 16) |(scanLine[ii] << 8)| (scanLine[ii]) | 0xFF000000;
+                            int[] scanLine = ((ImageLineInt)lint).getScanline();
+                            nextPixel = (scanLine[ii] << 16) |(scanLine[ii] << 8)| (scanLine[ii]);/* | 0xFF000000;*/
                         }
                         image.setRGB(ii-wProgress,r-hProgress,nextPixel);
                     }
@@ -93,41 +93,43 @@ public class ImageManipulator {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                wProgress += maxWidth;
+                wProgress += minWidth;
             }
-            hProgress += maxHeight;
+            hProgress += minHeight;
         }
     }
 
-//    private int[] getBestCombi(int w, int h, int a){
-//        int[] bestCombi = {1,1,1,w*h};//p,n,a,nb
-//        long nbT = (long) w *h;
-//        long rect_aT=1;
-//        for(int p =1;p<=w;p++){
-//            if(nbT<p) break;
-//            int rect_w = (int) Math.ceil(w/p);
-//            if(a<rect_w)continue;
-//            for(int n = 1; n<=h;n++){
-//                int nb = p*n;
-//                if(nbT<nb) continue;
-//                int rect_h = (int) Math.ceil(h/n);
-//                long rect_a;
-//                rect_a =(long)rect_w*rect_h;
-//                if(a<rect_a)continue;
-//                if(nb<nbT||(nb == nbT && rect_a<rect_aT)){
-//                    bestCombi[0]=p;
-//                    bestCombi[1]=n;
-//                    nbT = nb;
-//                    rect_aT = rect_a;
-//                }
-//            }
-//        }
-//        return bestCombi;
-//    }
+/*
+    private int[] getBestCombi(int w, int h, int a){
+        int[] bestCombi = {1,1,1,w*h};//p,n,a,nb
+        long nbT = (long) w *h;
+        long rect_aT=1;
+        for(int p =1;p<=w;p++){
+            if(nbT<p) break;
+            int rect_w = (int) Math.ceil(w/p);
+            if(a<rect_w)continue;
+            for(int n = 1; n<=h;n++){
+                int nb = p*n;
+                if(nbT<nb) continue;
+                int rect_h = (int) Math.ceil(h/n);
+                long rect_a;
+                rect_a =(long)rect_w*rect_h;
+                if(a<rect_a)continue;
+                if(nb<nbT||(nb == nbT && rect_a<rect_aT)){
+                    bestCombi[0]=p;
+                    bestCombi[1]=n;
+                    nbT = nb;
+                    rect_aT = rect_a;
+                }
+            }
+        }
+        return bestCombi;
+    }
+*/
 
     public boolean isInLoadedImages(int[] pos){
-        for(int i =0; i<4;i++){
-            if(Arrays.equals(pos,this.loadedImgPosCache[i])){
+        for (int[] posCache : this.loadedImgPosCache) {
+            if (Arrays.equals(pos, posCache)) {
                 return true;
             }
         }
@@ -147,12 +149,12 @@ public class ImageManipulator {
     }
 
     private void putInCache(int[] pos, BufferedImage image){
-        for(int i =0; i<3;i++){
+        for(int i =0; i< loadedImgPosCache.length-1;i++){
             this.loadedImgPosCache[i] = this.loadedImgPosCache[i+1];
             this.loadedImagesCache[i] = this.loadedImagesCache[i+1];
         }
-        this.loadedImgPosCache[3] = pos;
-        this.loadedImagesCache[3] = image;
+        this.loadedImgPosCache[loadedImgPosCache.length-1] = pos;
+        this.loadedImagesCache[loadedImagesCache.length-1] = image;
     }
 
     public int getRGB(int x,int y){
@@ -160,7 +162,7 @@ public class ImageManipulator {
         if(!isInLoadedImages(pos)){
             putInCache(pos,getImage(x,y));
         }
-        for(int i = 0; i<4;i++){
+        for(int i = 0; i< loadedImgPosCache.length;i++){
             if(Arrays.equals(pos,this.loadedImgPosCache[i])){
                 int localX = x%maxWidth;
                 int localY = y%maxHeight;
